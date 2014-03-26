fae15ea628fd6b1cdf6b49fa7e431872be5acb18
收真 <shumaF@osamushin-no-MacBook-Air.local>
2014-03-27 00:11:14 +0900

    create project

---
+++ src/main/java/jp/shuma/multiclient/Main.java
@@ -0,0 +1,18 @@
+package jp.shuma.multiclient;
+
+/**
+ * メインクラス．
+ * とりあえずHello・・・
+ * @since 0.1
+ * @version 0.1
+ * @param shuma
+ */
+public class Main {
+	/**
+	 * メインメソッド．
+	 * @since 0.1
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello MultiClient");
+	}
+}
---
+++ src/main/java/jp/shuma/multiclient/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * multiclient(マルチクライアント)
+ * Twitter，mixi，Facebookなど
+ * SNSを一括して扱える．Clientの
+ * 作成を目指す．
+ * 
+ *  ---- version ----
+ *  0.1	 create project! 2014/3/27 
+ *        - Main.java 作成
+ */
+package jp.shuma.multiclient;
